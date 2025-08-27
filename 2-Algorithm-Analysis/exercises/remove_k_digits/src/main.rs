9a8deefcd6bab5188b2cc5ba957c20f1cdb751b2
mickaelyoshua <mickaelyoshua@gmail.com>
2025-08-27 19:40:25 -0300

    more

--- 2-Algorithm-Analysis/exercises/remove_k_digits/src/main.rs
+++ 2-Algorithm-Analysis/exercises/remove_k_digits/src/main.rs
@@ -1,22 +1,30 @@
-use std::f64;
+use std::f32;
 
 fn remove_kdigits(num: &str, k: usize) -> String {
-    let mut min = f64::INFINITY;
+    if num.len() <= k {
+        return String::from("0");
+    }
+
+    let mut min = f32::INFINITY;
 
     for i in 0..=num.len()-k {
-        let value = format!("{}{}", &num[..i], &num[i+k..]).parse().unwrap();
+        let value = format!("{}{}", &num[..i], &num[i+k..])
+            .parse()
+            .unwrap();
         if value < min {
             min = value;
         }
-
     }
     min.to_string()
 }
 
 fn main() {
-    let num = "1432219";
-    let k = 3;
-
-    let result = remove_kdigits(num, k);
-    assert_eq!(result, "1219");
+    let nums = ["1432219","10200","10"];
+    let ks = [3,1,2];
+    let results = ["1219","200","0"];
+    
+    for (i, (num, k)) in nums.iter().zip(ks).enumerate() {
+        let result = remove_kdigits(num, k);
+        assert_eq!(result, results[i]);
+    }
 }
