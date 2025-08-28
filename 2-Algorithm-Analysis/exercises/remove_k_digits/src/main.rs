c798d8ab11aa02ef8186f2b4cd89c0778f97a69b
mickaelyoshua <mickaelyoshua@gmail.com>
2025-08-28 08:42:53 -0300

    more

--- 2-Algorithm-Analysis/exercises/remove_k_digits/src/main.rs
+++ 2-Algorithm-Analysis/exercises/remove_k_digits/src/main.rs
@@ -1,30 +1,46 @@
-use std::f32;
-
-fn remove_kdigits(num: &str, k: usize) -> String {
-    if num.len() <= k {
+fn remove_kdigits(num: &str, k: i32) -> String {
+    let num_len = num.len() as i32;
+    if k >= num_len {
         return String::from("0");
     }
 
-    let mut min = f32::INFINITY;
-
-    for i in 0..=num.len()-k {
-        let value = format!("{}{}", &num[..i], &num[i+k..])
-            .parse()
-            .unwrap();
-        if value < min {
-            min = value;
-        }
+    if k < 1 || num_len > 10_i32.pow(5) {
+        panic!("k or number length out of bounds.");
     }
-    min.to_string()
+    
+    let mut result: Vec<char> = Vec::new();
+    let mut k = k;
+
+    for digit in num.chars() {
+        while !result.is_empty() && k > 0 && *result.last().unwrap() > digit {
+            result.pop();
+            k -= 1;
+        }
+        result.push(digit);
+    }
+
+    while k > 0 {
+        result.pop();
+        k -= 1;
+    }
+
+    let first_index = result.iter().position(|&c| c != '0').unwrap_or(result.len());
+    let result_str = result[first_index..].iter().collect::<String>();
+
+    if result_str.is_empty() {
+        String::from("0")
+    } else {
+        result_str
+    }
 }
 
 fn main() {
-    let nums = ["1432219","10200","10"];
-    let ks = [3,1,2];
-    let results = ["1219","200","0"];
+    let nums = ["1432219","10200","10","9","112"];
+    let ks = [3,1,2,1,1];
+    let results = ["1219","200","0","0","11"];
     
-    for (i, (num, k)) in nums.iter().zip(ks).enumerate() {
-        let result = remove_kdigits(num, k);
-        assert_eq!(result, results[i]);
+    for ((num, k), expected) in nums.iter().zip(ks.iter()).zip(results.iter()) {
+        let result = remove_kdigits(num, *k);
+        assert_eq!(result, *expected);
     }
 }
