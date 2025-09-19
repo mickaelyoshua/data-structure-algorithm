9f732fda13226a87a1cb7ba30840b390dc8dd660
mickaelyoshua <mickaelyoshua@gmail.com>
2025-09-19 08:33:28 -0300

    validate bst

--- 3-Data-Structures/exercises/validate_bst/src/main.rs
+++ 3-Data-Structures/exercises/validate_bst/src/main.rs
@@ -1,4 +1,7 @@
-type Link = Option<Box<TreeNode>>;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type Link = Option<Rc<RefCell<TreeNode>>>;
 
 struct TreeNode {
     val: i32,
@@ -7,22 +10,40 @@ struct TreeNode {
 }
 
 fn main() {
-    let left_tree = Some(Box::new(TreeNode {
+    let left_tree = Some(Rc::new(RefCell::new(TreeNode {
         val: 1,
         left: None,
         right: None,
-    }));
-    let right_tree = Some(Box::new(TreeNode {
+    })));
+    let right_tree = Some(Rc::new(RefCell::new(TreeNode {
         val: 3,
         left: None,
         right: None,
-    }));
+    })));
 
-    let root = TreeNode {
+    let root = Some(Rc::new(RefCell::new(TreeNode {
         val: 2,
         left: left_tree,
         right: right_tree,
-    };
+    })));
 
+    println!("is valid: {}", is_valid_bts(root));
+}
 
+fn is_valid_bts(root: Link) -> bool {
+    validate(&root, i64::MIN, i64::MAX)
+}
+
+fn validate(root: &Link, min: i64, max: i64) -> bool {
+    if let Some(node_rc) = root {
+        let node = node_rc.borrow();
+        let node_val = node.val as i64;
+        if node_val <= min || node_val >= max {
+            false
+        } else {
+            validate(&node.left, min, node_val) && validate(&node.right, node_val, max)
+        }
+    } else {
+        true
+    }
 }
